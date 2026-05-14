8c1e560057d9275efc0541cae28abda5c72b4a7a
Shaan Purewal <shaan@debian>
2026-05-14 00:15:28 -0400

    Naive: exploit symmetry

--- main.go
+++ main.go
@@ -3,19 +3,34 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
+	"runtime"
 )
+
+/*
+
+   NOTE: This implementation assumes that the input samples are real-valued,
+   this is exploited when taking advantage of coefficient symmetries.
+
+   NAIVE Discrete Fourier Transform (w/ Inverse)
+   (Shaan Purewal)
+   
+*/
 
 type Coeff struct {
 	cos float64
 	sin float64
 }
 
-const SAMPLE_SIZE = 8
-const PI = math.Pi
+const (
+	SAMPLE_SIZE = 10_000
+	PI = math.Pi
+	NYQUIST = SAMPLE_SIZE / 2 + 1
+)
 
 func main() {
 	var samples [SAMPLE_SIZE]float64
-	var coefficients [SAMPLE_SIZE]Coeff
+	var coefficients [NYQUIST]Coeff
 	var recovered [SAMPLE_SIZE]float64
 
 	// Aquire (gen)
@@ -47,6 +62,8 @@ func generateSamples(samples []float64, fn func(int) float64) {
 	for i := range samples {
 		samples[i] = fn(i)
 	}
+	name := runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
+	fmt.Printf("\n%d samples generated w/ '%s'\n", len(samples), name)
 }
 
 func DFT(samples []float64, coefficients []Coeff) {
@@ -55,28 +72,36 @@ func DFT(samples []float64, coefficients []Coeff) {
 		for s_idx := range samples {
 			ratio := float64(s_idx) / float64(len(samples))
 			angle := 2.0 * PI * float64(f_idx) * ratio
-
+			
 			coeff.cos += samples[s_idx] * math.Cos(angle)
 			coeff.sin -= samples[s_idx] * math.Sin(angle)
 		}
 		coefficients[f_idx] = coeff
 	}
+	fmt.Println("Finished performing Fourier Transform")
 }
 
 func IDFT(coefficients []Coeff, recovered []float64) {
+	N := len(recovered)
 	for r_idx := range recovered {
 		real := 0.0
 		for f_idx := range coefficients {
-			ratio := float64(r_idx) / float64(len(recovered))
+			ratio := float64(r_idx) / float64(N)
 			angle := 2.0 * PI * float64(f_idx) * ratio
 			cos_A, sin_B := coefficients[f_idx].cos, coefficients[f_idx].sin
 
-			real += cos_A * math.Cos(angle)
-			real -= sin_B * math.Sin(angle)
+			term := cos_A * math.Cos(angle) - sin_B * math.Sin(angle)
+			evenNyquist := (N % 2 == 0) && (f_idx == N/2)
+			
+			if f_idx > 0 && !evenNyquist{
+				term *= 2
+			}
+			real += term
 		}
-		real /= float64(len(recovered))
+		real /= float64(N) // Normalize
 		recovered[r_idx] = real
 	}
+	fmt.Println("Finished performing Inverse Fourier Transform")
 }
 
 func meanSquared(expected []float64, actual []float64) float64 {
